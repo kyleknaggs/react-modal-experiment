062cf4a6c15c415ce83de9c127d756b13807362a
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-06 16:17:30 -0400

    Add content and styles to modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,21 +10,34 @@ export default function Home() {
 
   const style = {
     overlay: {
-      backgroundColor: 'red',
-      opacity: '0.5',
+      backgroundColor: 'rgb(255, 0, 0, 0.5)', /* red */
+    },
+    content: {
+      backgroundColor: 'rgb(0, 153, 0)', /* green */
     },
   };
 
-  return (
+  const modal = (
     <div>
-      <button onClick={handleClick}>Open</button>
       <ReactModal
         isOpen={isModalOpen}
         style={style}
       >
         <button onClick={handleClick}>Close</button>
+        <h2>Modal title</h2>
         <p>Modal built using react-modal</p>
       </ReactModal>
+    </div>
+  );
+
+  return (
+    <div>
+      {modal}
+      <div>
+        <h1>Page title</h1>
+        <button onClick={handleClick}>Open</button>
+        <p>Lorem ipsum</p>
+      </div>
     </div>
   );
 }
