a313bef16a3ed33bd7c88cd2a9178c31e4a84cb4
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-06 16:03:15 -0400

    Build a simple modal using react-modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import ReactModal from 'react-modal';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleClick = function () {
+    setModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Open</button>
+      <ReactModal
+        isOpen={isModalOpen}
+      >
+        <button onClick={handleClick}>Close</button>
+        <p>Modal built using react-modal</p>
+      </ReactModal>
+    </div>
+  );
 }
